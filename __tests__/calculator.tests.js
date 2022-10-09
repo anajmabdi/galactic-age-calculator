f1eb70ca491b03afd637bc810d08a25407d639f8
Najma Abdi <anajmabdi@gmail.com>
2022-10-09 14:30:21 -0700

    it will calculate how many years the user has left on Mercury

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -1,4 +1,5 @@
-import Calculator from '../src/Calculator.js';
+import { Calculator } from '../src/Calculator.js';
+import { yearsLeft } from '../src/Calculator.js';
 
 describe('Calculator', () => {
   test('should build a Calculator object that contains a set of properties', () => {
@@ -14,16 +15,28 @@ describe('Calculator', () => {
   test('should convert earth age to Mercury age', () => {
     let calculator = new Calculator(22);
     calculator.convertToMercury();
-    expect(Math.floor((calculator.earthAge*365)/88)).toEqual(91);
+    expect(Math.floor((calculator.earthAge * 365) / 88)).toEqual(91);
     expect(calculator.mercuryAge).toEqual(91);
-    });
-    
+  });
+  
   test('should calculate the remainder of their life on Mercury', () => {
     let calculator = new Calculator(22);
-    calculator.convertToMercury();
-    calculator.yearsLeft();
+    let calculator2 = new Calculator(1);
 
-    expect(calculator.mercuryAge).toEqual(-18);
-    console.log(calculator.mercuryAge);
-    });
+    calculator.convertToMercury();
+    calculator.remainderMercury();
+    calculator2.convertToMercury();
+    calculator2.remainderMercury();
+
+    expect(calculator.mercuryAge).toEqual(91)
+    expect(calculator.humanLifeSpan).toEqual(73);
+    expect(calculator.yearsLeftInMercury).toEqual(0);
+
+    expect(calculator2.mercuryAge).toEqual(4)
+    expect(calculator2.humanLifeSpan).toEqual(73);
+    expect(calculator2.yearsLeftInMercury).toEqual(69);
+  });
 });
+
+
+
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,10 +1,14 @@
-export default class Calculator {
+export class Calculator {
   constructor(age) {
     this.earthAge = age;
     this.mercuryAge = 0; // 88 days
+    this.yearsLeftInMercury = 0
     this.venusAge = 0; // 225 days
+    this.yearsLeftInVenus = 0
     this.marsAge = 0; // 687 days
+    this.yearsLeftInMars = 0
     this.jupiterAge = 0; // 4333 days
+    this.yearsLeftInJupiter = 0
     this.humanLifeSpan = 73; //rounded World's average lifespan (72.98)
   }
 
@@ -12,5 +16,12 @@ export default class Calculator {
     let age = this.earthAge;
       let conversion = Math.floor((age * 365) / 88);
       this.mercuryAge = conversion + this.mercuryAge;
+  }
+  remainderMercury() {
+    if (this.mercuryAge < this.humanLifeSpan) {
+      this.yearsLeftInMercury = this.humanLifeSpan - this.mercuryAge;
+    }else {
+      return 0;
+    }
   }
 }
