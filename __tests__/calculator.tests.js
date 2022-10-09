e542db918061908338e7c566804ce4f959a300be
Najma Abdi <anajmabdi@gmail.com>
2022-10-09 13:58:48 -0700

    passed red phase

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -10,12 +10,20 @@ describe('Calculator', () => {
     expect(calculator.jupiterAge).toEqual(0);
     expect(calculator.humanLifeSpan).toEqual(73);
   });
-  test('should convert earth age to mercury age', () => {
+
+  test('should convert earth age to Mercury age', () => {
     let calculator = new Calculator(22);
     calculator.convertToMercury();
-
     expect(Math.floor((calculator.earthAge*365)/88)).toEqual(91);
     expect(calculator.mercuryAge).toEqual(91);
     });
     
+  test('should calculate the remainder of their life on Mercury', () => {
+    let calculator = new Calculator(22);
+    calculator.convertToMercury();
+    calculator.yearsLeft();
+
+    expect(calculator.mercuryAge).toEqual(-18);
+    console.log(calculator.mercuryAge);
+    });
 });
